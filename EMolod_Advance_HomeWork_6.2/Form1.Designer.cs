5e583e9e20b22b20d9e5064783a32d6253d61435
NikitaSmetanin <NikitaSmetanin1705@gmail.com>
2023-12-20 01:32:47 +0200

    Добавьте файлы проекта.

---
+++ EMolod_Advance_HomeWork_6.2/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace EMolod_Advance_HomeWork_6._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTextBox = new System.Windows.Forms.TextBox();
+            this.historyTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Location = new System.Drawing.Point(13, 124);
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.ReadOnly = true;
+            this.mainTextBox.Size = new System.Drawing.Size(297, 26);
+            this.mainTextBox.TabIndex = 0;
+            this.mainTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // historyTextBox
+            // 
+            this.historyTextBox.Location = new System.Drawing.Point(12, 12);
+            this.historyTextBox.Multiline = true;
+            this.historyTextBox.Name = "historyTextBox";
+            this.historyTextBox.ReadOnly = true;
+            this.historyTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.historyTextBox.Size = new System.Drawing.Size(298, 95);
+            this.historyTextBox.TabIndex = 1;
+            this.historyTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 441);
+            this.Controls.Add(this.historyTextBox);
+            this.Controls.Add(this.mainTextBox);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mainTextBox;
+        private System.Windows.Forms.TextBox historyTextBox;
+    }
+}
+
---
+++ EMolod_Advance_HomeWork_6.2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace EMolod_Advance_HomeWork_6._2
+{
+    public partial class Form1 : Form
+    {
+        double number = 1;
+        public Form1()
+        {
+            InitializeComponent();
+
+            int x = 20;
+            int y = 160;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    this.createButton(new Point(x + 70 * j, y + 70 * i), number.ToString(), new Size(50, 50));
+                    number++;
+                }
+            }
+            this.createButton(new Point(20, 370), "0", new Size(190, 50));         
+
+        }
+
+        private System.Windows.Forms.Button createButton(Point point, string text, Size size)
+        {
+            System.Windows.Forms.Button button = new System.Windows.Forms.Button();
+
+            button.Font = new Font("Comic Sans MS", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button.Location = point;
+            button.Name = "numberButton";
+            button.Size = size;
+            button.TabIndex = 0;
+            button.Text = text;
+            button.UseVisualStyleBackColor = true;
+            button.Click += numberButton_Click;
+
+            this.Controls.Add(button);
+
+            return button;
+        }
+        // Функція обробки числових кнопок
+        private void numberButton_Click(object sender, EventArgs e)
+        {
+            mainTextBox.Text += ((System.Windows.Forms.Button)sender).Text;
+        }
+        // Функція обробки кнопок операцій +, -, /, *
+        private void operationButton1_Click(object sender, EventArgs e)
+        {
+            this.number = Convert.ToDouble(mainTextBox.Text);
+            mainTextBox.Text = string.Empty;
+        }
+        // Функція обробки кнопки =
+        private void equalButton_Click(object sender, EventArgs e)
+        {
+            mainTextBox.Text = (this.number + Convert.ToDouble(mainTextBox.Text)).ToString();
+        }
+        // Функція обробки кнопки очистки
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            this.number = 0;
+            mainTextBox.Text = string.Empty;
+        }
+    }
+}
